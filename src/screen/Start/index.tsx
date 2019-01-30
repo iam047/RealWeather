9f0d3b2c34e7105e983df37951c44cb30e21af8b
iam047 <iam047iam@gmail.com>
2019-01-30 21:57:27 +0200

    used react-native config

--- src/screen/Start/index.tsx
+++ src/screen/Start/index.tsx
@@ -1,6 +1,7 @@
 import React                            from 'react';
 import { connect }                      from 'react-redux';
 import { Text, View, TouchableOpacity } from 'react-native';
+import config                           from 'react-native-config';
 
 import { IStartProps, IStartState } from './types';
 import { navBack }                  from '../../actions';
@@ -8,8 +9,7 @@ import styles                       from './styles';
 
 class StartScreen extends React.Component<IStartProps, IStartState> {
     componentDidMount() {
-        //fetch('http://api.openweathermap.org/data/2.5/weather?lat=49.26&lon=32.3&APPID=380333d47d591b073e01150c5943197a')
-        fetch('https://api.darksky.net/forecast/e6c9afe1d5a53031082a853ae00ce790/49.42854,32.06207')
+        fetch(`https://api.darksky.net/forecast/${config.API_DARKSKY_NET_KEY}/49.42854,32.06207`)
             .then(response => response.json())
             .then(date => console.log(JSON.stringify({ date }, null, 4)))
             .catch(error => {
